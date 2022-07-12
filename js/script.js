a3ec9b513786523933936cf64058d9689f42663c
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-07-12 13:21:45 -0500

    Completed do_guess function and added onclick to html

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let num = Math.floor(Math.random() * 20) + 1;
+
+function do_guess() {
+    let guess = Number(document.getElementById('guess').value);
+    let message = document.getElementById('message');
+    
+    if(guess == num) {
+        message.innerHTML = "You got it!";
+    }
+    else if (guess < num) {
+        message.innerHTML = "No, try a higher number.";
+    }
+    else {
+        message.innerHTML = "No, try a lower number.";
+    }
+}
